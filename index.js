d7b01e117236d5a37fd78a5ac0f0aa48c09baeae
ZZ H <HZZcode@outlook.com>
2025-04-28 00:07:44 +0800

    control blocks & comments

--- index.js
+++ index.js
@@ -44,6 +44,9 @@ class TextAreaManager {
     currentLineCount() {
         return this.content.substring(0, this.start).split(/\r?\n/).length - 1;
     }
+    currentColumn() {
+        return this.currentLineFrontContent().length;
+    }
     currentLine() {
         return this.lines[this.currentLineCount()];
     }
@@ -96,24 +99,34 @@ class TagScanner {
         return -1;
     }
 }
+const info = document.getElementById('info');
 let characters = new AutoComplete();
-let tags = new AutoComplete(['[Character]', '[Jump]', '[Anchor]']);
+let tags = new AutoComplete(['[Character]', '[Jump]', '[Anchor]', '[Select]', '[Switch]', '[Case]', '[End]']);
 let anchorCompleter = new AutoComplete();
 let characterScanner = new TagScanner('[Character]');
 let anchorScanner = new TagScanner('[Anchor]');
 textarea.addEventListener('keydown', processKeyDown);
 textarea.addEventListener('mouseup', jumpTo);
+textarea.addEventListener('input', updateInfo);
+textarea.addEventListener('selectionchange', updateInfo);
+function updateInfo(_) {
+    let manager = new TextAreaManager();
+    info.innerText = `Line ${manager.currentLineCount()}, Column ${manager.currentColumn()}`;
+}
 function processKeyDown(event) {
     let key = event.key;
     characters.list = characterScanner.scanList();
     if (key === 'Tab')
         autoComplete(event);
+    if (event.ctrlKey && key === '/')
+        comment(event);
 }
 function autoComplete(event) {
     let manager = new TextAreaManager();
     event.preventDefault();
-    if (manager.currentLine().startsWith('[') && (manager.currentLine().search(']') == -1
-        || manager.currentLineFrontContent().trim().endsWith(']')))
+    let line = manager.currentLine();
+    let front = manager.currentLineFrontContent();
+    if (line.startsWith('[') && (line.search(']') == -1 || front.trim().endsWith(']')))
         completeTag(event);
     else {
         completeCharaterName(event);
@@ -133,7 +146,12 @@ function completeTag(_) {
     let manager = new TextAreaManager();
     let line = manager.currentLine();
     let tag = tags.complete('[' + line.replaceAll('[', '').replaceAll(']', ''), line.search(']') === -1);
-    if (tag != undefined) manager.edit(manager.currentLineCount(), tag)
+    if (tag !== undefined) manager.edit(manager.currentLineCount(), tag);
+    if (tag === '[Case]') {
+        manager.edit(manager.currentLineCount(), tag + ':');
+        textarea.selectionStart--;
+        textarea.selectionEnd--;
+    }
 }
 function completeJump(_) {
     let manager = new TextAreaManager();
@@ -150,14 +168,82 @@ function jumpTo(event) {
     let manager = new TextAreaManager();
     let front = manager.currentLineFrontContent();
     let line = manager.currentLine();
-    if (front.search(':') === -1 && line.search(':') !== -1) {
+    if (front.search(':') === -1 && line.search(':') !== -1 && !line.trim().startsWith('[')) {
         let name = line.substring(0, line.search(':'));
         let pos = characterScanner.scanLine(name);
         if (pos !== -1) manager.jumpTo(pos);
     }
-    if (front.search('[Jump]') !== -1) {
+    else if (front.trim().startsWith('[Jump]')) {
         let anchor = line.replace('[Jump]', '').trim();
         let pos = anchorScanner.scanLine(anchor);
         if (pos !== -1) manager.jumpTo(pos);
     }
+    else if (line.trim().startsWith('[End]')) {
+        let index = manager.currentLineCount();
+        let blocks = scanControlBlocks();
+        for (let block of blocks) {
+            if (block.endPos === index) {
+                manager.jumpTo(block.startPos);
+                return;
+            }
+        }
+    }
+    else if (line.trim().startsWith('[Case]')) {
+        let index = manager.currentLineCount();
+        let blocks = scanControlBlocks();
+        for (let block of blocks) {
+            if (block.casesPosList.some(i => i === index)) {
+                manager.jumpTo(block.startPos);
+                return;
+            }
+        }
+    }
+}
+function comment(_) {
+    let manager = new TextAreaManager();
+    let line = manager.currentLine();
+    if (line.trim().startsWith('//'))
+        line = line.replace('//', '').trim();
+    else line = '// ' + line;
+    manager.edit(manager.currentLineCount(), line);
+}
+class ControlBlock {
+    startPos;
+    casesPosList;
+    endPos;
+    constructor(startPos, casesPosList, endPos) {
+        this.startPos = startPos;
+        this.casesPosList = casesPosList;
+        this.endPos = endPos;
+    }
+}
+function scanControlBlocks() {
+    let manager = new TextAreaManager();
+    let controlTags = ['[Select]', '[Switch]'];
+    let isControlTag = line => controlTags.some(value => line.startsWith(value));
+    let ans = [];
+    let stack = [];
+    for (let [index, line] of manager.lines.entries()) {
+        if (isControlTag(line)) {
+            stack.push(new ControlBlock(index, [], -1));
+        }
+        if (line.startsWith('[Case]')) {
+            if (stack.length === 0)
+                console.error(`Error: [Case] tag out of control block at line ${index}`);
+            else {
+                stack[stack.length - 1].casesPosList.push(index);
+            }
+        }
+        if (line.startsWith('[End]')) {
+            if (stack.length === 0)
+                console.error(`Error: Extra [End] found at line ${index}`);
+            else {
+                let block = stack.pop();
+                block.endPos = index;
+                ans.push(block);
+            }
+        }
+    }
+    console.assert(stack.length === 0, `Error: Control Block ([Select]-[End] or [Switch]-[End]) not closed`);
+    return ans;
 }
